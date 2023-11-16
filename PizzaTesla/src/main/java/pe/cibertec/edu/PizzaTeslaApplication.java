4221c96381e3d83f37d1d2058d89d9ba76e399e2
rasec1106 <elch.herrera@gmail.com>
2023-11-15 23:00:16 -0500

    Create initial backend project

---
+++ PizzaTesla/src/main/java/pe/cibertec/edu/PizzaTeslaApplication.java
@@ -0,0 +1,13 @@
+package pe.cibertec.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaTeslaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaTeslaApplication.class, args);
+	}
+
+}
