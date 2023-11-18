bed1c5ffaebf6ca3a1b210c58d7520e6f29263df
FranDelgadillo <frandelgadilloso@gmail.com>
2023-11-17 21:11:10 -0500

    Second Commit

--- PizzaTesla/src/main/java/pe/cibertec/edu/service/ProductService.java
+++ PizzaTesla/src/main/java/pe/cibertec/edu/service/ProductService.java
@@ -1,5 +1,14 @@
 package pe.cibertec.edu.service;
 
-public interface ProductService {
+import pe.cibertec.edu.model.Product;
 
+import java.util.List;
+
+public interface ProductService {
+    List<Product> getAllProducts();
+    Product getProductById(int id);
+    Product createProduct(Product product);
+    Product updateProduct(Product product);
+    boolean deleteProduct(int id);
+    List<Product> getProductsByCategory(int categoryId);
 }
--- PizzaTesla/src/main/java/pe/cibertec/edu/service/impl/ProductServiceImpl.java
+++ PizzaTesla/src/main/java/pe/cibertec/edu/service/impl/ProductServiceImpl.java
@@ -1,5 +1,59 @@
 package pe.cibertec.edu.service.impl;
 
-public class ProductServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pe.cibertec.edu.model.Product;
+import pe.cibertec.edu.repository.ProductRepository;
+import pe.cibertec.edu.service.ProductService;
 
+import java.util.List;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductServiceImpl(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    @Override
+    public Product getProductById(int id) {
+        return productRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Product createProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    @Override
+    public Product updateProduct(Product product) {
+        if (productRepository.existsById(product.getId())) {
+            return productRepository.save(product);
+        }
+        return null; // Handle the case where the product does not exist
+    }
+
+    @Override
+    public boolean deleteProduct(int id) {
+        if (productRepository.existsById(id)) {
+            productRepository.deleteById(id);
+            return true;
+        }
+        return false; // Handle the case where the product does not exist
+    }
+
+    @Override
+    public List<Product> getProductsByCategory(int categoryId) {
+        // Implement your custom logic here
+        // Example: return productRepository.findByCategoryId(categoryId);
+        return null;
+    }
 }
